58b66149ae2b75f42a650616a0dc7982ffffbd23
gaule222 <vothicuong123@gmail.com>
2025-10-12 22:46:03 +0700

    10%

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './NewCollections.css'
+const NewCollections =() =>{
+    return(
+    <div className="newcollections">
+
+    </div>
+    )
+}
+
+export default NewCollections
